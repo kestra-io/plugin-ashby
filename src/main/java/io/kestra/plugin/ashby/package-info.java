495dc216f3ae72083484d65a223c37ffdf0df1e5
jymaire <jmaire@kestra.io>
2026-09-17 15:55:46 +0200

    chore: initial scaffold cleanup for plugin-ashby

---
+++ src/main/java/io/kestra/plugin/ashby/package-info.java
@@ -0,0 +1,8 @@
+@PluginSubGroup(
+    title = "Ashby",
+    description = "Ashby plugin for Kestra",
+    categories = PluginSubGroup.PluginCategory.DATA
+)
+package io.kestra.plugin.ashby;
+
+import io.kestra.core.models.annotations.PluginSubGroup;
